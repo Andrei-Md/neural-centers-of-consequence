d36ecf6bcf10b8a8847156856d165b52faba3c55
Andrei Moldoveanu <moldoveanud.andrei@yahoo.com>
2022-05-25 13:43:09 +0200

    data gathering

--- pdf/main.py
+++ pdf/main.py
@@ -18,27 +18,32 @@ def main():
     #     os.path.abspath("reward magnitude/assessed processing of reward receipt (outcome phase)"))
     # pdf_folder_path = Path(
     #     os.path.abspath("reward magnitude/reward expectancy (anticipation phase)"))
-    pdf_folder_path = Path(
-        os.path.abspath("all"))
+
+    #all
+    # pdf_folder_path = Path(
+    #     os.path.abspath("all"))
+    pdf_folder_path = Path(os.path.abspath("pdfs/todo"))
+    # pdf_folder_path = Path(os.path.abspath("pdfs/WM"))
+
     docs = Pdfc(pdf_folder_path, method="fitz")
 
     ## rename files
-    # docs.rename_files(max_chars=40, ignore_files=["~"])
+    docs.rename_files(max_chars=40, ignore_files=["~"])
 
     ## generate csv
-    # docs.generate_csv(avoid=["-"], ignore_files=["~"])
+    docs.generate_csv(avoid=["-"], ignore_files=["~"])
 
     # # # docs.create_xml("content.xml")
-    df = docs.process_csv(ignore_files=["gen_"])
-    #
+    # df = docs.process_csv(ignore_files=["gen_"])
+
     # # ## process data
     # df = Util.process_coordinates(df, conversion_type='tal2mni', columns_name=['X(R)', 'Y(A)', 'Z(S)'])
-    df = Util.process_coordinates(df, conversion_type='none', columns_name=['X(R)', 'Y(A)', 'Z(S)'])
-    #
+    # df = Util.process_coordinates(df, conversion_type='none', columns_name=['X(R)', 'Y(A)', 'Z(S)'])
+
     # # # save df to csv
-    csv_path = Path(join(os.path.abspath("reward magnitude"), "final_coordinates-no_conversion.csv"))
-    csv = Csv.Csv(csv_path, create=True)
-    csv.create_csv_from_pd(df, header=True, index=False)
+    # csv_path = Path(join(os.path.abspath("reward magnitude"), "final_coordinates-no_conversion.csv"))
+    # csv = Csv.Csv(csv_path, create=True)
+    # csv.create_csv_from_pd(df, header=True, index=False)
 
 
 if __name__ == '__main__':
